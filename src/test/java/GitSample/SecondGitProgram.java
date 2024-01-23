0b1e3658049b638299f1f43be6cc764d3f70beb5
HP <HP@LAPTOP-1R0L68IE>
2024-01-23 11:01:04 +0530

    Second Git Program

---
+++ src/test/java/GitSample/SecondGitProgram.java
@@ -0,0 +1,12 @@
+package GitSample;
+
+public class SecondGitProgram {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
+		System.out.println("My name is Nikita");
+
+	}
+
+}
