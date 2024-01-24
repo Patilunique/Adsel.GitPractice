719d6de3b4aee82354c1ece1224ade149186235b
HP <HP@LAPTOP-1R0L68IE>
2024-01-24 11:42:33 +0530

    contact created committed

---
+++ src/test/java/GitSample/ContactCreated.java
@@ -0,0 +1,14 @@
+package GitSample;
+
+public class ContactCreated {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi");
+		System.out.println("contact created");
+
+	
+
+	}
+
+}
